cbbe1907aeb3cc3e10d92ab91e374cfe2777f787
rchui <ryan.w.chui@gmail.com>
2017-01-26 13:00:27 -0600

    Initial test

---
+++ test.java
@@ -0,0 +1,8 @@
+import org.jblas.*;
+
+public class test {
+  public static void main(String[] args) {
+    double[][] inital = new double[2][2];
+    System.out.println(Arrays.deepToString(initial));
+  }
+}
